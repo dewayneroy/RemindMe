2993cfada35f0fdc891c1127761a7f7e519defb6
DeWayne Johnson Jr <waynejohnson1226@gmail.com>
2018-03-23 13:43:43 -0400

    Update break_time.py

--- break_time.py
+++ break_time.py
@@ -14,6 +14,6 @@ break_count = 0
 
 print("This program started on " +time.ctime())
 while (break_count < total_breaks):
-    time.sleep(2700) 
+    time.sleep(7200) 
     webbrowser.open("https://www.youtube.com/watch?v=Eegftbcv9Ug") 
     break_count = break_count + 1
